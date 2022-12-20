8f9b3250f7774f1573ad9fafcf6ce06e05a1fce9
0xFF00XX <gwynblade313@gmail.com>
2022-12-20 20:11:27 +0000

    old update

--- API/WebPoll/poll95.js
+++ API/WebPoll/poll95.js
@@ -5,10 +5,13 @@
 // get values time, ip, Group date
 //
 //set fields to value that was submitted.
-// var ip = "10.0.0.93";
-// var groupName = "";
+var ip = "10.0.0.93";
+var groupName = "DEL_98_Test";
+var startTime = 1669852800
+var endTime = 1670479200
 
 
 function main() {
-  alert("dssdf");
+  // var url =  "http://"+ip+":8581/odata/api/groups?$top=50&$skip=0&top=" + str(numberOfEntries)+"&&resolution=RATE&starttime="+str(startTime)+"&endtime="+str(endTime)+"&$format=json&$expand=portmfs&$select=ID,Name,portmfs/Timestamp,portmfs/im_BitsIn,portmfs/im_BitsOut&$filter=((Name eq '"+groupName+"'))"
+  alert("zpple" + 1);
 }
