e1ef71ed567da8ff16eea45736279e4eee6d56f1
0xFF00XX <gwynblade313@gmail.com>
2023-09-20 16:23:03 +0100

    proxy added. receiving info

--- API/WebPoll/poll95.js
+++ API/WebPoll/poll95.js
@@ -4,8 +4,9 @@ const config = {
   enableTime: true,
   time_24hr : true,
   // maxDate: "today",
-  defaultDate: currDateTime(),
-  maxDate: currDateTime(),
+  //below function explained below
+  defaultDate: currDateTime(0,0,0,0,0),
+  maxDate: currDateTime(0,0,0,0,0),
   dateFormat: "d-m-Y H:i",
   altInput: true,
   altFormat: "F j, Y @ H:i",
@@ -20,8 +21,8 @@ const endFlatPicker = flatpickr(endFlatInput,config);
 const startFlatPicker = flatpickr(startFlatInput, {
   enableTime: true,
   time_24hr : true,
-  defaultDate: currDateTime(),
-  maxDate: currDateTime(),
+  defaultDate: currDateTime(0,0,0,0,5),
+  maxDate: currDateTime(0,0,0,0,0),
   dateFormat: "d-m-Y H:i",
   altInput: true,
   altFormat: "F j, Y @ H:i",
@@ -30,20 +31,21 @@ const startFlatPicker = flatpickr(startFlatInput, {
       endFlatPicker.set("minDate", dateStr);
 
   // Constrain the maxDate of the start date picker to today's date
-      startFlatPicker.set("maxDate", currDateTime());
+      startFlatPicker.set("maxDate", currDateTime(0,0,0,0,0));
   },
   });
 
 
 
-
-function currDateTime(){
+//request time. args -- substract from current year,month etc to use in Last Hour or Last month selections.
+//example currDateTime(0,0,0,-4,0) -- should return date four hours back.
+function currDateTime(year,month,date,hour,mins){
   var now = new Date();
-  var currentYear = now.getFullYear();
-  var currentMonth = now.getMonth();
-  var currentDay = now.getDate();
-  var currentHour = now.getHours();
-  var currentMinute = now.getMinutes();
+  var currentYear = now.getFullYear() - year;
+  var currentMonth = now.getMonth() - month;
+  var currentDay = now.getDate() - date;
+  var currentHour = now.getHours() - hour;
+  var currentMinute = now.getMinutes() - mins;
   // Create a date object with the current date and time
   console.log();
 
@@ -56,7 +58,7 @@ const selector = document.getElementById('selectTimeRange');
 const flatTimes = document.getElementById('flatTimes');
 selector.addEventListener('change', function() {
   // Check if the "Custom" option is selected
-  if (selector.value === 'Custom') {
+  if (selector.value === 'custom') {
     flatTimes.style.display = "block"; // Show time pickers
   } else {
     flatTimes.style.display = "none"; // Hide time pickers
@@ -67,21 +69,52 @@ selector.addEventListener('change', function() {
 
 document.getElementById("submitForm").addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent the default form submission
+
+  const errorMessageContainer = document.getElementById("error-message");
+  errorMessageContainer.textContent = "";
+  let isValid = false;
   let startEpoch =0 ;
   let endEpoch =0;
 
-  if(selector.value === "Last hour"){
+  if(selector.value === "hour"){
     console.log("last hour");
+  }
+  else if (selector.value === "4hours"){
+    console.log("last 4 hours");
+  }
+  else if (selector.value === "day"){
+    console.log("last 24 hours");
+  }
+  else if (selector.value === "7days"){
+    console.log("last 7 days");
+  }
+  else if (selector.value === "month"){
+    console.log("last month");
   }
   else{
     startEpoch = (new Date(startFlatPicker.selectedDates).getTime())/1000;
     endEpoch = (new Date(endFlatPicker.selectedDates).getTime())/1000;
+
+    //testing for minimum
+    if(((endEpoch - startEpoch) < 300) && ((endEpoch - startEpoch) >= 0)){
+      console.log("Less than minimum");
+      errorMessageContainer.textContent = "End time is less than 5 minutes from Start Time.";
+    }
+    else if((endEpoch - startEpoch) < 0){
+
+      errorMessageContainer.textContent = "End time below zero!";
+    }
+    else{
+      isValid = true;
+    }
+  }
+  //perform calculations and plot with valid time
+  if(isValid){
+    requestData(startEpoch, endEpoch, "max_im_BitsIn", "DEL_98_Test")
+    // graph(startEpoch, endEpoch, "max_im_BitsIn", "BIP")
+    console.log(startEpoch);
   }
 
-
-  requestData(startEpoch, endEpoch, "max_im_BitsIn", "BIP")
-  graph(startEpoch, endEpoch, "max_im_BitsIn", "BIP")
-  console.log(startEpoch);
 
 });
 
@@ -89,9 +122,45 @@ function graph(epochStart, epochEnd, metric, group){
 
 }
 function requestData(epochStart, epochEnd, metric, group){
-  const ip = "10.0.0.91";
-  url= "http://"+ip+":8581/odata/api/groups?$top=20000&skip=0&top=20000&resolution=RATE&starttime="+epochStart+"&endtime="+epochEnd+"&$format=json&$expand=portmfs&$select=ID,Name,portmfs/Timestamp,portmfs/"+metric+"&$filter=((Name eq '"+group+"'))"
+  const ip = "127.0.0.1";
+  const port = "8000"
+  url= "http://"+ip+":"+port+"/odata/api/groups?$top=20000&skip=0&top=20000&resolution=RATE&starttime="+epochStart+"&endtime="+epochEnd+"&$format=json&$expand=portmfs&$select=ID,Name,portmfs/Timestamp,portmfs/"+metric+"&$filter=((Name eq '"+group+"'))"
+
+  const username = 'admin';
+  const password = '!DataOverEdge!';
+  const base64Credentials = btoa(`${username}:${password}`);
+  const headers = new Headers({
+    'Authorization': `Basic ${base64Credentials}`,
+    'Content-Type': 'application/json', // Set the content type based on the expected response
+  });
+
+  const request = new Request(url, {
+
+  });
+
+  fetch(request)
+  .then(response => {
+    // Check if the response status is OK (status code 200)
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    else{
+      console.log('Received JSON data:', response);
+      var element = document.getElementById("graph");
+      element.textContent = JSON.stringify(response);
+      // Parse the JSON response
+      // return response.json();
+      // console.log(response.json());
+    }
+
+  })
+  .catch(error => {
+    // Handle errors such as network issues or authentication problems
+    console.error('Error:', error);
+  });
+
+
   console.log(JSON.stringify(url, null, 4));
 }
 // debug
-console.log(currDateTime());
+console.log(currDateTime(0,0,0,0,0));
---
+++ API/WebPoll/proxy.py
@@ -0,0 +1,49 @@
+import http.server
+import socketserver
+import urllib.request
+import base64
+# Define the proxy server configuration
+PORT = 8000  # Choose a port for your proxy server
+TARGET_HOST = "10.0.0.93"  # Replace with the base URL of the external API
+TARGET_PORT = 8581  # Default HTTP port
+
+class ProxyHandler(http.server.CGIHTTPRequestHandler):
+        # Allow all origins by setting CORS headers for both OPTIONS and GET requests
+    def do_OPTIONS(self):
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header("mode", "no-cors")
+        self.end_headers()
+
+
+    def do_GET(self):
+        # Build the target URL
+        target_url = f"http://{TARGET_HOST}:{TARGET_PORT}{self.path}"
+        # print(target_url)
+        # Create a proxy request to the target URL
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        super().end_headers()
+
+        try:
+            username = "admin"
+            password = "!DataOverEdge!"
+            auth_header = {"Authorization": f"Basic {base64.b64encode(f'{username}:{password}'.encode()).decode()}"}
+            request = urllib.request.Request(target_url, headers=auth_header)
+            # Fetch content from the target server
+            with urllib.request.urlopen(request) as response:
+                content = response.read()
+                print(type(content))
+                print(content)
+                # content = content.decode('utf-8')
+
+                self.wfile.write(content)
+        except Exception as e:
+            print("Error:", e)
+
+# Create the proxy server
+with socketserver.TCPServer(("", PORT), ProxyHandler) as httpd:
+    print(f"Proxy server is running on port {PORT}")
+    httpd.serve_forever()
